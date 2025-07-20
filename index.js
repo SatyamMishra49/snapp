79844ae7bba66a89caa4df060daf454747e252a9
SatyamMishra49 <skm28112001@gmail.com>
2025-07-20 15:29:11 +0530

    minor fix

--- index.js
+++ index.js
@@ -15,6 +15,31 @@ app.get('/v1/apis' , (req, res) => {
 });
 
 
+app.get('/v1/apis/test', async (req, res) => {
+    try {
+        API_KEY = ''
+        const headers = {
+            'Accept':'application/json','x-api-key': API_KEY
+        };
+        const response = await fetch('https://api.manyapis.com/v1-get-short-url',
+        {
+        method: 'GET',
+        headers: headers
+        })
+        .then(function(res) {
+            return res.json();
+        }).then(function(body) {
+            console.log(body);
+        });
+
+    }
+    catch(e) {
+        console.error(e)
+        logger.error('Error occurred while fetching API', { error: e.message, stack: e.stack });
+    }
+})
+
+
 app.listen(5000, () => {
     logger.info('Server is running on port 5000');
 });
