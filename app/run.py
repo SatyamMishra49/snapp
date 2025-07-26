2faa67bd1385a4d2b464588e5ffe62ad88277457
SatyamMishra49 <skm28112001@gmail.com>
2025-07-26 22:28:29 +0530

    feat: added routes

--- app/run.py
+++ app/run.py
@@ -1,5 +1,4 @@
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -22,5 +21,18 @@ GET long_urls/
 def get_long_urls():
     return jsonify({"message": "long urls"}), 200
 
+''''
+POST urls/
+'''
+@app.route('/v1/url', methods=['POST'])
+def create_url():
+    data = request.get_json()
+    original_url = data.get('url')
+
+    if original_url:
+        # save the URL to a database
+        return jsonify({"message": "URL created", "url": original_url}), 201
+    else:
+        return jsonify({"error": "Invalid URL"}), 400
 
 app.run(debug=True, port=5000)
